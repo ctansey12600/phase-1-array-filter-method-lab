35ae9faf42b8e473409f6030bb0e6a1eed70477f
caseytansey <ctansey12600@outlook.com>
2021-08-19 15:53:04 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(array, string){
+   let result = array.filter(function(drivers){
+        return drivers.toLowerCase() === `${string}`.toLowerCase()
+    })
+    return result
+}
+
+function fuzzyMatch(array, string){
+    let result = array.filter(function(drivers){
+        return drivers.startsWith(`${string}`)
+    })
+    return result
+}
+
+function matchName(array, string){
+    let result = array.filter(function(drivers){
+        return drivers.name === `${string}`
+    })
+    return result
+}
